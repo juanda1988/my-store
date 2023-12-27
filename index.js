6c3cd52c3ba9cc378954c4019650c2d5a50f48be
juanda1988 <jdmarti12@gmail.com>
2023-12-27 13:09:53 -0500

    segundo avance del curso

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { faker } = require('@faker-js/faker');
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
+  }
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000,
+  });
+});
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    });
+  } else {
+    res.send('No hay parámetros');
+  }
+});
+
+app.get('Categories/:categoryId/products/productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  });
 });
 
 app.listen(port, () => {
